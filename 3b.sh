c540cdb25cdf2d2422a8e3bbcd849f2c75dc724f
Navyasngowda <100507775+Navyasngowda@users.noreply.github.com>
2022-05-02 16:35:21 +0530

    Add files via upload

---
+++ 3b.sh
@@ -0,0 +1,29 @@
+echo "Enter a passward"
+read p1
+echo "Enter the confirmed passward"
+stty -echo
+read p2
+stty echo
+while [ "$p1" != "$p2"
+do
+echo "Passward does not match.Enter the currect passward"
+stty -echo
+rerad p2
+stty echo
+done
+clear
+echo "------terminal locked------------"
+echo "Enter the passward to unlock"
+stty -echo
+read p3
+stty echo
+while [ "$p1" != "$p3" ]
+do
+clear
+echo "-------terminal locked---------------"
+echo "Passward does not match.Re-enter the passward"
+read p3
+done
+echo "---------terminal unlocked-------------"
+
+
