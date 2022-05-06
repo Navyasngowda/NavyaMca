45bb432b313981bfc9090a506a460aab885f2b2f
Navyasngowda <100507775+Navyasngowda@users.noreply.github.com>
2022-05-06 18:20:12 +0700

    Add files via upload

---
+++ p2.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<stdlib.h>
+int cost[15][15];
+int n,mincost,v1=0,v2=0;
+void findMin()
+{
+int edgewt=99,i,j;
+for(i=0;i<n;i++){
+  for(j=0;j<n;j++){
+     if(cost[i][j]>0&&cost[i][j]<<edgecost
+
+i
+
+void 
+
+void main()
+{
+int i,j;
+printf("Enter the number of vertices");
+scanf("%d",&n);
+printf("Enter the undirected graphs as adjacency matrix\n");
+for(i=0;i<n;i++){
+   for(j=0;j<n;j++){
+       scanf("%d"\t",&cos[i];
+       }
+}
+for(i=0;i<n;i++){
+    for(j=0;j<n;j++){
+        printf("%d\t",cos[i][j]);
+}
+printf("\n");
+}
+kruskal()
